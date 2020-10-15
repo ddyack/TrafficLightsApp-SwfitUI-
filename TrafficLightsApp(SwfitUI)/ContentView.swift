5892df7ff79807154318e259e93a0f3be9bf210e
ddyack <ddyack@yandex.ru>
2020-10-15 14:11:06 +0300

    Initial Commit

---
+++ TrafficLightsApp(SwfitUI)/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  TrafficLightsApp(SwfitUI)
+//
+//  Created by ddyack on 15.10.2020.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ TrafficLightsApp(SwfitUI)/TrafficLightsApp_SwfitUI_App.swift
@@ -0,0 +1,17 @@
+//
+//  TrafficLightsApp_SwfitUI_App.swift
+//  TrafficLightsApp(SwfitUI)
+//
+//  Created by ddyack on 15.10.2020.
+//
+
+import SwiftUI
+
+@main
+struct TrafficLightsApp_SwfitUI_App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
